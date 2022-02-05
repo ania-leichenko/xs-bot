983805832d184a967363d377e025d9e74170a925
unknown <ania.leichnko@gmail.com>
2022-02-05 17:36:04 +0200

    BWS-28: + tenants table

--- backend/src/common/enums/db/table-name.enum.ts
+++ backend/src/common/enums/db/table-name.enum.ts
@@ -2,6 +2,7 @@ enum TableName {
   USERS = 'users',
   MASTERS = 'entity_access_management.masters',
   PERMISSIONS = 'entity_access_management.permissions',
+  TENANTS = 'entity_access_management.tenants',
   MIGRATIONS = 'migrations',
 }
 
