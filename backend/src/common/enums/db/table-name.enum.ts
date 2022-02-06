5faba4de2a5f8fb76669867829c637d6b8275388
unknown <ania.leichnko@gmail.com>
2022-02-06 16:35:10 +0200

    BWS-28: + workers table

--- backend/src/common/enums/db/table-name.enum.ts
+++ backend/src/common/enums/db/table-name.enum.ts
@@ -3,6 +3,7 @@ enum TableName {
   MASTERS = 'entity_access_management.masters',
   PERMISSIONS = 'entity_access_management.permissions',
   TENANTS = 'entity_access_management.tenants',
+  WORKERS = 'entity_access_management.workers',
   MIGRATIONS = 'migrations',
 }
 
