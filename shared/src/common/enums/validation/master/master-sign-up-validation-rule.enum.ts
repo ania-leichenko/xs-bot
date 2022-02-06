1c171e6fdf295bf795c14bc5e5edc3331ac72caa
Bogdan Degtiar <jonejei@ukr.net>
2022-02-06 14:44:15 +0200

    BWS-15: + as const to MasterSignUpValidationRule

--- shared/src/common/enums/validation/master/master-sign-up-validation-rule.enum.ts
+++ shared/src/common/enums/validation/master/master-sign-up-validation-rule.enum.ts
@@ -2,6 +2,6 @@ const MasterSignUpValidationRule = {
   NAME_MIN_LENGTH: 4,
   NAME_FIRST_CHARTER: /^[a-zA-Z].+$/,
   PASSWORD_MIN_LENGTH: 6,
-};
+} as const;
 
 export { MasterSignUpValidationRule };
