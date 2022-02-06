ee7ade00f4996b7fb1553018edc44b831c3c405e
Bogdan Degtiar <96128765+junojei@users.noreply.github.com>
2022-02-06 09:24:25 +0200

    Update shared/src/common/enums/validation/master/master-sign-up.validation-message.ts
    Should I add regex to MasterSignUpValidationRule?
    But it will be const.
    
    Co-authored-by: Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>

--- shared/src/common/enums/validation/master/master-sign-up.validation-message.ts
+++ shared/src/common/enums/validation/master/master-sign-up.validation-message.ts
@@ -2,7 +2,7 @@ const MasterSignUpValidationMessage = {
   EMAIL_REQUIRE: 'Email is required',
   EMAIL_WRONG: 'Email is wrong',
   NAME_REQUIRE: 'Name is required',
-  NAME_MIN_LENGTH: 'Name must have at least 4 characters',
+  NAME_MIN_LENGTH: 'Name must have at least ${MasterSignUpValidationRule. NAME_MIN_LENGTH} characters',
   NAME_FIRST_CHARTER: 'Name must start with a letter',
   PASSWORD_REQUIRE: 'Name is required',
   PASSWORD_MIN_LENGTH: 'Password must have at least 4 characters',
