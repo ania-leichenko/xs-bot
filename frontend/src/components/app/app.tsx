e9023ffae72529aa41b3a860e84e7efdc8c347ca
Bogdan Degtiar <jonejei@ukr.net>
2022-02-06 23:28:20 +0200

    BWS-15: - starting menu

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,30 +1,11 @@
 import { FC } from 'react';
-import logo from 'assets/img/logo.svg';
-import { Link, Routes, Route } from 'components/common/common';
-import { useLocation } from 'hooks/hooks';
+import { Routes, Route } from 'components/common/common';
 import { AppRoute } from 'common/enums/enums';
 import { Auth } from 'components/auth/auth';
 
 const App: FC = () => {
-  const { pathname } = useLocation();
-
   return (
     <>
-      <img src={logo} className="App-logo" width="30" alt="logo" />
-
-      <ul className="App-navigation-list">
-        <li>
-          <Link to={AppRoute.ROOT}>Root</Link>
-        </li>
-        <li>
-          <Link to={AppRoute.SIGN_IN}>Sign in</Link>
-        </li>
-        <li>
-          <Link to={AppRoute.SIGN_UP}>Sign up</Link>
-        </li>
-      </ul>
-      <p>Current path: {pathname}</p>
-
       <div>
         <Routes>
           <Route path={AppRoute.SIGN_IN} element={<Auth />} />
