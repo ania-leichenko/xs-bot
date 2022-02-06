866d575a5e203a18f8564cb30f5dee0b0536ed3e
sofia0602 <sofia.prusik@gmail.com>
2022-02-06 20:42:39 +0200

    bws-20: + handleSignInSubmit in sign-wrapper-form

--- frontend/src/components/auth/components/sign-wrapper-form/sign-wrapper-form.tsx
+++ frontend/src/components/auth/components/sign-wrapper-form/sign-wrapper-form.tsx
@@ -1,16 +1,19 @@
 import style from './sign-wrapper-form.module.scss';
 import { SignInForm } from '../components';
 import { MasterSignInDto } from 'common/types/types';
+import { auth as authActions } from 'store/actions';
+import { useAppDispatch } from 'hooks/hooks';
 import Logo from './Logo.svg';
 const SignWrapperForm: React.FC = () => {
-  const onSubmit: any = (data: MasterSignInDto) => {
-    return data;
+  const dispatch = useAppDispatch();
+  const handleSignInSubmit = (payload: MasterSignInDto): void => {
+    dispatch(authActions.signIn(payload));
   };
   return (
     <>
       <div className={style.wrapper}>
         <img src={Logo} alt="logo" className={style.logo} />
-        <SignInForm onSubmit={onSubmit} />
+        <SignInForm onSubmit={handleSignInSubmit} />
       </div>
     </>
   );
