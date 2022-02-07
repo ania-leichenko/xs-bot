ba8a1497de1b88d0611fd62d9ce957fafc4b2d56
sofia0602 <sofia.prusik@gmail.com>
2022-02-07 09:51:02 +0200

    bws-20: - sign-wrapper-form

--- frontend/src/components/auth/components/components.ts
+++ frontend/src/components/auth/components/components.ts
@@ -1,3 +1,2 @@
 export { SignInForm } from './sign-in-form/sign-in-form';
 export { SignUpForm } from './sign-up-form/sign-up-form';
-export { SignWrapperForm } from './sign-wrapper-form/sign-wrapper-form';
