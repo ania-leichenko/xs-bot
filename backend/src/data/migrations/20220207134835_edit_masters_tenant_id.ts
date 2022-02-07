9cf97bdc7b46d87f4b4192199731ef1207a6f74c
unknown <ania.leichnko@gmail.com>
2022-02-07 16:43:01 +0200

    BWS-41: * export in edit_masters_tenant_id file

--- backend/src/data/migrations/20220207134835_edit_masters_tenant_id.ts
+++ backend/src/data/migrations/20220207134835_edit_masters_tenant_id.ts
@@ -3,14 +3,16 @@ import { TableName } from '~/common/enums/enums';
 
 const TABLE_NAME = TableName.MASTERS;
 
-export async function up(knex: Knex): Promise<void> {
+async function up(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, function (table) {
     table.dropNullable('tenant_id');
   });
 }
 
-export async function down(knex: Knex): Promise<void> {
+async function down(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, function (table) {
     table.setNullable('tenant_id');
   });
 }
+
+export { up, down };
