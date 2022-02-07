f3a36ca5723dd712660d9c7efac73475f743f923
Donymak <donyhp2@gmail.com>
2022-02-07 10:42:59 +0200

    BWS-32: + added table wrapper with title and background

--- frontend/src/components/common/table/table.tsx
+++ frontend/src/components/common/table/table.tsx
@@ -6,9 +6,10 @@ import styles from './table.module.scss';
 type Props = {
   columns: Column[];
   data: unknown[];
+  title: string;
 };
 
-const Table: React.FC<Props> = ({ columns, data }) => {
+const Table: React.FC<Props> = ({ columns, data, title }) => {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
       columns: columns as Column<Record<string, string>>[],
@@ -16,41 +17,46 @@ const Table: React.FC<Props> = ({ columns, data }) => {
     });
 
   return (
-    <table {...getTableProps()} className={styles.clientSideTable}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr
-            className={styles.tableHeaderRow}
-            {...headerGroup.getHeaderGroupProps()}
-          >
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                className={styles.tableHeaderCell}
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr className={styles.tableRow} {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td {...cell.getCellProps()} className={styles.tableCell}>
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
+    <div className={styles.tableWrapper}>
+      <div className={styles.tableHat}>
+        <div className={styles.tableTitle}>{title}</div>
+      </div>
+      <table {...getTableProps()} className={styles.clientSideTable}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr
+              className={styles.tableHeaderRow}
+              {...headerGroup.getHeaderGroupProps()}
+            >
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  className={styles.tableHeaderCell}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr className={styles.tableRow} {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()} className={styles.tableCell}>
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
