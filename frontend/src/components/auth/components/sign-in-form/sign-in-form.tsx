1cdca4e5966ee1522bb8fca6a138784cf4f830f0
bohdanshcherba <73034554+bohdanshcherba@users.noreply.github.com>
2022-02-13 10:24:30 +0200

    BWS-66 * added transition-duration to hover btn

--- frontend/src/components/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/components/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,12 +1,7 @@
 import { FC } from 'react';
 import { eamMasterSignIn as masterSignInValidationSchema } from 'validation-schemas/validation-schemas';
-import {
-  AppRoute,
-  ButtonStyle,
-  ButtonType,
-  InputType,
-} from 'common/enums/enums';
-import { useAppForm, useState } from 'hooks/hooks';
+import { AppRoute, ButtonType, InputType } from 'common/enums/enums';
+import { useAppForm } from 'hooks/hooks';
 import { getNameOf } from 'helpers/helpers';
 import { EAMMasterSignInRequestDto } from 'common/types/types';
 import { Button, Input, Link, PasswordInput } from 'components/common/common';
@@ -23,12 +18,6 @@ const SignInForm: FC<Props> = ({ onSubmit }) => {
       defaultValues: DEFAULT_LOGIN_PAYLOAD,
       validationSchema: masterSignInValidationSchema,
     });
-
-  const [isClicked, setIsClicked] = useState(false);
-
-  const clickHandler = (): void => {
-    setIsClicked(!isClicked);
-  };
 
   return (
     <>
@@ -55,12 +44,7 @@ const SignInForm: FC<Props> = ({ onSubmit }) => {
             errors={errors}
           />
         </div>
-        <Button
-          type={ButtonType.SUBMIT}
-          label="Sign In"
-          onClick={clickHandler}
-          btnStyle={isClicked ? ButtonStyle.OUTLINED : ButtonStyle.FILLED}
-        />
+        <Button type={ButtonType.SUBMIT} label="Sign In" />
       </form>
     </>
   );
