46b16917a0ae6ad6132c43fd6d7bd0f60172a6fd
bohdanshcherba <73034554+bohdanshcherba@users.noreply.github.com>
2022-02-14 15:23:03 +0200

    BWS-55 * get tenantId from state user

--- frontend/src/store/group-creation/actions.ts
+++ frontend/src/store/group-creation/actions.ts
@@ -10,14 +10,15 @@ const create = createAsyncThunk<
   EAMGroupCreateResponseDto,
   EAMGroupCreateRequestDto,
   AsyncThunkConfig
->(ActionType.CREATE, async (registerPayload, { extra }) => {
+>(ActionType.CREATE, async (registerPayload, { getState, extra }) => {
   const { groupApi } = extra;
 
-  const tenantId = 'd94b2c68-289d-467c-914f-ababcdc8fcaa';
+  const { auth } = getState();
+  const tenantId = auth.user?.tenantId;
 
   const request: EAMGroupCreateRequestDto = {
     name: registerPayload.name,
-    tenantId,
+    tenantId: tenantId ?? '',
   };
   return await groupApi.create(request);
 });
