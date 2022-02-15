22fb353d7e31c1dfcc71f3ca9f1256fd2df15a66
tbezrukava <tetiana.bezrukava@gmail.com>
2022-02-15 16:51:03 +0200

    BWS-59: + groups table to eam page

--- frontend/src/components/eam/eam.tsx
+++ frontend/src/components/eam/eam.tsx
@@ -1,9 +1,31 @@
 import { FC } from 'react';
 import { Link } from 'components/common/common';
 import { AppRoute } from 'common/enums/enums';
+import { useAppDispatch, useAppSelector, useEffect } from 'hooks/hooks';
+import { eam as eamActions } from 'store/actions';
+import { GroupsTable } from './components/groups-table/groups-table';
 import styles from './eam.module.scss';
 
 const EAM: FC = () => {
+  const dispatch = useAppDispatch();
+  const { id: tenantId } = useAppSelector(({ app }) => ({
+    id: app.tenant?.id,
+  }));
+
+  useEffect(() => {
+    if (!tenantId) {
+      return;
+    }
+
+    dispatch(
+      eamActions.loadGroups({
+        tenantId: tenantId as string,
+        from: 0,
+        count: 5,
+      }),
+    );
+  }, [dispatch, tenantId]);
+
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>
@@ -13,6 +35,7 @@ const EAM: FC = () => {
       <div className={styles.link}>
         <Link to={AppRoute.EAM_CREATE_WORKER}>Add User</Link>
       </div>
+      <GroupsTable />
     </div>
   );
 };
