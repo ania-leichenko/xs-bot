5162a6ff2bc31ff4375afeab50c8a58b4fd83c59
Bogdan Degtiar <jonejei@ukr.net>
2022-02-15 19:46:43 +0200

    BWS-70: * AuthApiPath

--- shared/src/common/enums/enums.ts
+++ shared/src/common/enums/enums.ts
@@ -1,4 +1,10 @@
-export { ApiPath, MastersApiPath, TenantsApiPath, EAMApiPath } from './api/api';
+export {
+  ApiPath,
+  AuthApiPath,
+  MastersApiPath,
+  TenantsApiPath,
+  EAMApiPath,
+} from './api/api';
 export { CustomExceptionName } from './exceptions/exceptions';
 export { ContentType } from './file/file';
 export { HttpCode, HttpHeader, HttpMethod } from './http/http';
