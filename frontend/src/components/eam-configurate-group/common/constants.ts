d9a104dd1bd26a12e62b0fba0ec8210c6dcc9995
bohdanshcherba <73034554+bohdanshcherba@users.noreply.github.com>
2022-02-19 14:09:17 +0200

    BWS-96: * fix

--- frontend/src/components/eam-configurate-group/common/constants.ts
+++ frontend/src/components/eam-configurate-group/common/constants.ts
@@ -3,6 +3,7 @@ import { EAMGroupConfigurateRequestDto } from 'common/types/types';
 const DEFAULT_GROUP_PAYLOAD: EAMGroupConfigurateRequestDto = {
   name: '',
   workersIds: [],
+  permissionsIds: [],
 };
 
 export { DEFAULT_GROUP_PAYLOAD };
