dd03bb04e6cbe23341142f36524d739262a28931
Bogdan Degtiar <jonejei@ukr.net>
2022-02-19 18:08:35 +0200

    BWS-21 * export in migration

--- backend/src/data/migrations/20220219134401_update_functions_table.ts
+++ backend/src/data/migrations/20220219134401_update_functions_table.ts
@@ -4,7 +4,7 @@ import { getFormattedISODate } from '~/helpers/helpers';
 
 const TABLE_NAME = TableName.FUNCTIONS;
 
-export async function up(knex: Knex): Promise<void> {
+async function up(knex: Knex): Promise<void> {
   const dateNowISO = getFormattedISODate(new Date());
 
   return knex.schema.table(TABLE_NAME, function (table) {
@@ -13,9 +13,11 @@ export async function up(knex: Knex): Promise<void> {
   });
 }
 
-export async function down(knex: Knex): Promise<void> {
+async function down(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, function (table) {
     table.dropColumn('updated_at');
     table.setNullable('created_by');
   });
 }
+
+export { up, down };
