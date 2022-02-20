6c05f20ce9588d37660c684987e896ee2a51be99
Donymak <donyhp2@gmail.com>
2022-02-20 16:37:11 +0200

    BWS-24: + types and table

--- frontend/src/common/types/bs-space/bs-space.ts
+++ frontend/src/common/types/bs-space/bs-space.ts
@@ -2,4 +2,7 @@ export {
   type BSSpaceCreateRequestDto,
   type BSSpaceCreateResponseDto,
   type BSSpaceCreateFormDto,
+  type BSSpaceGetResponseItemDto,
+  type BSSpaceGetResponseDto,
+  type BSSpaceGetRequestParamsDto,
 } from 'bws-shared/common/types/types';
