3bbffb82e49e675b747d2711529e47fc46d89285
Donymak <donyhp2@gmail.com>
2022-02-20 10:53:23 +0200

    BWS-24: - spaces from reducer state

--- frontend/src/store/bs-space-create/reducer.ts
+++ frontend/src/store/bs-space-create/reducer.ts
@@ -1,16 +1,13 @@
-import { BSSpaceCreateResponseDto } from 'common/types/types';
 import { DataStatus } from 'common/enums/enums';
 import { createReducer } from '@reduxjs/toolkit';
 import { createSpace } from './actions';
 
 type State = {
   dataStatus: DataStatus;
-  spaces: BSSpaceCreateResponseDto[];
 };
 
 const initialState: State = {
   dataStatus: DataStatus.IDLE,
-  spaces: [],
 };
 
 const reducer = createReducer(initialState, (builder) => {
