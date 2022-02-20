731ee3541542cbdb469528446b45622774abbe34
tbezrukava <tetiana.bezrukava@gmail.com>
2022-02-20 10:30:20 +0200

    BWS-24: - key pairs table change

--- backend/src/data/migrations/20220219201116_edit_instances_table.ts
+++ backend/src/data/migrations/20220219201116_edit_instances_table.ts
@@ -5,13 +5,13 @@ const TABLE_NAME = TableName.INSTANCES;
 
 async function up(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, (table) => {
-    table.renameColumn('keyPairsId', 'keyPairId');
+    table.renameColumn('key_pairs_id', 'key_pair_id');
   });
 }
 
 async function down(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, (table) => {
-    table.renameColumn('keyPairId', 'keyPairsId');
+    table.renameColumn('key_pair_id', 'key_pairs_id');
   });
 }
 
--- backend/src/data/migrations/20220219202732_edit_instances_table.ts
+++ backend/src/data/migrations/20220219202732_edit_instances_table.ts
@@ -5,7 +5,11 @@ const TABLE_NAME = TableName.INSTANCES;
 
 async function up(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, (table) => {
-    table.uuid('tenant_id').references('id').inTable(TableName.TENANTS);
+    table
+      .uuid('tenant_id')
+      .references('id')
+      .inTable(TableName.TENANTS)
+      .notNullable();
   });
 }
 
