89aff95b2b99701bbdc275aa50a022c859af0676
sofia0602 <sofia.prusik@gmail.com>
2022-02-22 10:45:09 +0200

    bws-107: * tenantId on worker

--- backend/src/data/migrations/20220222084102_edit_worker_tenant_id.ts
+++ backend/src/data/migrations/20220222084102_edit_worker_tenant_id.ts
@@ -1,17 +1,17 @@
 import { Knex } from 'knex';
 import { TableName } from '~/common/enums/enums';
 
-const TABLE_NAME = TableName.TENANTS;
+const TABLE_NAME = TableName.WORKERS;
 
 async function up(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, function (table) {
-    table.dropNullable('id');
+    table.dropNullable('tenant_id');
   });
 }
 
 async function down(knex: Knex): Promise<void> {
   return knex.schema.table(TABLE_NAME, function (table) {
-    table.setNullable('id');
+    table.setNullable('tenant_id');
   });
 }
 
