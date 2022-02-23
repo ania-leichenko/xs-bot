aa9ab6042f6762ca484edfcbe45898a88108510e
Bogdan Degtiar <jonejei@ukr.net>
2022-02-23 17:55:29 +0200

    BWS-21 + add delete-row-cell to table

--- frontend/src/components/slc/components/functions-table/helpers/get-rows.helper.ts
+++ frontend/src/components/slc/components/functions-table/helpers/get-rows.helper.ts
@@ -1,21 +1,30 @@
 import { FunctionsTableAccessor } from 'common/enums/enums';
 import { SLCFunctionGetResponseItemDto } from 'common/types/types';
+import { DeleteRowCell } from './cells/cells';
 import { getDistanceToDateNow } from 'helpers/helpers';
 
 type Row = {
   [FunctionsTableAccessor.FUNCTION_NAME]: string;
   [FunctionsTableAccessor.UPDATION_TIME]: string;
+  [FunctionsTableAccessor.ACTIONS]: JSX.Element;
 };
 
-const getRows = (slcFunctions: SLCFunctionGetResponseItemDto[]): Row[] => {
+const getRows = ({
+  slcFunctions,
+  onFunctionDelete,
+}: {
+  slcFunctions: SLCFunctionGetResponseItemDto[];
+  onFunctionDelete: (id: string) => void;
+}): Row[] => {
   return slcFunctions.map((item) => {
-    const { name, updatedAt } = item;
+    const { id, name, updatedAt } = item;
 
     return {
       [FunctionsTableAccessor.FUNCTION_NAME]: name,
       [FunctionsTableAccessor.UPDATION_TIME]: getDistanceToDateNow(
         new Date(updatedAt),
       ),
+      [FunctionsTableAccessor.ACTIONS]: DeleteRowCell(id, onFunctionDelete),
     };
   });
 };
