76271893d04eb82d1a237131783c1acbc23684fb
JunoJei <jonejei@ukr.net>
2022-03-01 13:27:13 +0200

    BWS-74: * fix name

--- backend/src/validation-schemas/validation-schemas.ts
+++ backend/src/validation-schemas/validation-schemas.ts
@@ -2,5 +2,5 @@ export { eamMasterSignUp, eamMasterSignIn } from './eam-master/eam-master';
 export { eamWorkerCreateBackend } from './eam-worker/eam-worker';
 export { eamGroupCreate } from './eam-group/eam-group';
 export { bsSpaceCreate } from './bs-space/bs-space';
-export { scInstanceCreate } from './sc-instance/sc-instanct';
+export { scInstanceCreate } from './sc-instance/sc-instance';
 export { slcFunctionCreate } from './slc-function/slc-function';
