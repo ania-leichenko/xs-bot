99272c3df4cd9b45fb93d19ab660ebb748a1f85b
ania-leichenko <ania.leichenko@gmail.com>
2022-03-02 08:15:18 +0200

    BWS-116: * merge conflicts

--- frontend/src/validation-schemas/eam-master/eam-master.ts
+++ frontend/src/validation-schemas/eam-master/eam-master.ts
@@ -1,5 +1,4 @@
 export {
   eamMasterSignUp,
   eamMasterSignIn,
-  eamWorkerSignIn,
 } from 'bws-shared/validation/eam-master/eam-master';
--- shared/src/validation/eam-master/eam-master.ts
+++ shared/src/validation/eam-master/eam-master.ts
@@ -1,3 +1,2 @@
 export { eamMasterSignUp } from './eam-master-sign-up/eam-master-sign-up.validation-schema';
 export { eamMasterSignIn } from './eam-master-sign-in/eam-master-sign-in.validation-schema';
-export { eamWorkerSignIn } from './eam-worker-sign-in/eam-worker.validation-schema';
