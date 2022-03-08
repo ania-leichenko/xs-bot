c2a7fcd44dc639eac9d881968f8aca518d4270d1
JunoJei <jonejei@ukr.net>
2022-03-08 17:49:58 +0200

    BWS-126: * update routes

---
+++ frontend/src/common/enums/app/additional-route.enum.ts
@@ -0,0 +1,5 @@
+enum AdditionalRoute {
+  ID = '/:id',
+}
+
+export { AdditionalRoute };
--- frontend/src/common/enums/app/app.ts
+++ frontend/src/common/enums/app/app.ts
@@ -1,4 +1,5 @@
 export { AppRoute } from './app-route.enum';
+export { AdditionalRoute } from './additional-route.enum';
 export { StorageKey } from './storage-key';
 export { DataStatus } from './data-status.enum';
 export { ENV } from './env.enum';
