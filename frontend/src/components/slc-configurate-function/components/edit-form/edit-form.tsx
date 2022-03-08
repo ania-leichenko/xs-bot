b65bbe1ff8b624d2bd446e2da484e2f016af56f3
JunoJei <jonejei@ukr.net>
2022-03-08 18:11:56 +0200

    BWS-126: + code editor to EditForm

--- frontend/src/components/slc-configurate-function/components/edit-form/edit-form.tsx
+++ frontend/src/components/slc-configurate-function/components/edit-form/edit-form.tsx
@@ -1,12 +1,27 @@
 import { FC } from 'react';
 import { AppRoute, ButtonStyle } from 'common/enums/enums';
 import { Button } from 'components/common/common';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 import styles from './styles.module.scss';
 
 const EditForm: FC = () => {
   return (
     <>
       <h3 className={styles.formTitle}>Edit Function</h3>;
+      <div className={styles.buttons}>
+        <div className={styles.button}>
+          <Button btnStyle={ButtonStyle.FILLED} label="Run" />
+        </div>
+        <div className={styles.button}>
+          <Button btnStyle={ButtonStyle.FILLED} label="Save" />
+        </div>
+      </div>
+      <CodeMirror
+        height="500px"
+        extensions={[javascript()]}
+        placeholder="Source code can't be empty."
+      />
       <div className={styles.buttons}>
         <div className={styles.button}>
           <Button
