05ba1851f0e7a157d72ba6462b2b983be77c2a12
JunoJei <jonejei@ukr.net>
2022-03-19 09:50:33 +0200

    BWS-97: * fix palceholder text

--- frontend/src/components/eam-configurate-group/components/workers-table/workers-table.tsx
+++ frontend/src/components/eam-configurate-group/components/workers-table/workers-table.tsx
@@ -37,7 +37,7 @@ const WorkersTable: FC<Props> = ({
         className={styles.table}
         columns={columns}
         data={data}
-        placeholder="No groups to display"
+        placeholder="No workers to display"
       />
     </div>
   );
