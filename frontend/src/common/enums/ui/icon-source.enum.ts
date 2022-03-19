c2d67aaab86190f7090b0b03cf956a13787855aa
JunoJei <jonejei@ukr.net>
2022-03-19 12:36:53 +0200

    BWS-105: + icons

--- frontend/src/common/enums/ui/icon-source.enum.ts
+++ frontend/src/common/enums/ui/icon-source.enum.ts
@@ -3,12 +3,18 @@ import deleteIcon from 'assets/img/delete-icon.svg';
 import editIcon from 'assets/img/edit-icon.svg';
 import keyIcon from 'assets/img/key-icon.svg';
 import reloadIcon from 'assets/img/reload-icon.svg';
+import successIcon from 'assets/img/success-icon.svg';
+import errorIcon from 'assets/img/error-icon.svg';
+import closeIcon from 'assets/img/close-icon.svg';
 
 const IconSource = {
   [IconName.TRASH]: deleteIcon,
   [IconName.GEAR]: editIcon,
   [IconName.KEY]: keyIcon,
   [IconName.RELOAD]: reloadIcon,
+  [IconName.SUCCESS]: successIcon,
+  [IconName.ERROR]: errorIcon,
+  [IconName.CLOSE]: closeIcon,
 } as const;
 
 export { IconSource };
