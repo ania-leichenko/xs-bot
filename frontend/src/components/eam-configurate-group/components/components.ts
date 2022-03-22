d7d2df0dc3831f9c17328133dfd7693b661b9351
bohdanshcherba <73034554+bohdanshcherba@users.noreply.github.com>
2022-03-22 14:42:35 +0200

    bws-95: * refactored configurate page

--- frontend/src/components/eam-configurate-group/components/components.ts
+++ frontend/src/components/eam-configurate-group/components/components.ts
@@ -1,4 +1,2 @@
 export { WorkersTable } from './workers-table/workers-table';
 export { PermissionsTable } from './permissions-table/permissions-table';
-export { CreateGroup } from './create-group/create-group';
-export { UpdateGroup } from './update-group/update-group';
--- frontend/src/components/eam-configurate-group/eam-configurate-group.tsx
+++ frontend/src/components/eam-configurate-group/eam-configurate-group.tsx
@@ -1,9 +1,28 @@
 import { FC } from 'react';
-import { useAppDispatch, useEffect, useAppSelector } from 'hooks/hooks';
-
+import { Button, Input } from 'components/common/common';
+import {
+  useAppDispatch,
+  useAppForm,
+  useAppSelector,
+  useEffect,
+  useSelectedItems,
+} from 'hooks/hooks';
+import { DEFAULT_GROUP_PAYLOAD } from './common/constants';
+import {
+  AppRoute,
+  ButtonStyle,
+  ButtonType,
+  InputType,
+} from 'common/enums/enums';
+import { getNameOf } from 'helpers/helpers';
 import { EAMGroupConfigurate as EAMGroupConfigurateActions } from 'store/actions';
-
-import { CreateGroup, UpdateGroup } from './components/components';
+import styles from './styles.module.scss';
+import {
+  EAMGroupConfigurateRequestDto,
+  EAMGroupUpdateRequestDto,
+} from 'common/types/types';
+import { eamGroupConfigurate as eamGroupConfigurateValidationSchema } from 'validation-schemas/validation-schemas';
+import { WorkersTable, PermissionsTable } from './components/components';
 
 const EAMConfigurateGroup: FC = () => {
   const dispatch = useAppDispatch();
@@ -38,7 +57,102 @@ const EAMConfigurateGroup: FC = () => {
     dispatch(EAMGroupConfigurateActions.getPermission());
   }, [dispatch, tenantId, group]);
 
-  return <div>{group ? <UpdateGroup group={group} /> : <CreateGroup />}</div>;
+  const { control, errors, handleSubmit, handleReset } =
+    useAppForm<EAMGroupConfigurateRequestDto>({
+      defaultValues: DEFAULT_GROUP_PAYLOAD,
+      validationSchema: eamGroupConfigurateValidationSchema,
+    });
+
+  const selectedWorkers = useSelectedItems<string>([]);
+  const selectedPermissions = useSelectedItems<string>([]);
+
+  useEffect(() => {
+    if (group) {
+      handleReset({ name: group.name });
+      selectedWorkers.handleReset(group.workersIds);
+      selectedPermissions.handleReset(group.permissionsIds);
+    }
+  }, [group]);
+
+  const handleFormSubmit = (payload: EAMGroupConfigurateRequestDto): void => {
+    if (group) {
+      const newPayload: EAMGroupUpdateRequestDto = {
+        id: group.id,
+        name: payload.name,
+        workersIds: selectedWorkers.selectedItems,
+        permissionsIds: selectedPermissions.selectedItems,
+      };
+      dispatch(EAMGroupConfigurateActions.update(newPayload));
+    } else {
+      const newPayload: EAMGroupConfigurateRequestDto = {
+        name: payload.name,
+        workersIds: selectedWorkers.selectedItems,
+        permissionsIds: selectedPermissions.selectedItems,
+      };
+      dispatch(EAMGroupConfigurateActions.create(newPayload));
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>
+        EAM - <br />
+        Entity Access Management
+      </h2>
+      <section className={styles.formWrapper}>
+        <h3 className={styles.formTitle}>Create Group</h3>
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <ul className={styles.inputGroups}>
+            <li className={styles.inputGroup}>
+              <h3 className={styles.inputGroupTitle}>Name the group</h3>
+              <div className={styles.inputWrapper}>
+                <Input
+                  type={InputType.TEXT}
+                  placeholder="Enter a name to identify this group"
+                  label=""
+                  name={getNameOf<EAMGroupConfigurateRequestDto>('name')}
+                  control={control}
+                  errors={errors}
+                />
+              </div>
+            </li>
+            <li>
+              <WorkersTable
+                selectedWorkers={selectedWorkers.selectedItems}
+                handleIsCheckedId={selectedWorkers.handleCheck}
+                handleRemoveWorkerId={selectedWorkers.handleRemove}
+                handleAddWorkerId={selectedWorkers.handleAdd}
+              />
+            </li>
+            <li>
+              <PermissionsTable
+                selectedPermissions={selectedPermissions.selectedItems}
+                handleIsCheckedPermissionId={selectedPermissions.handleCheck}
+                handleRemovePermissionId={selectedPermissions.handleRemove}
+                handleAddPermissionId={selectedPermissions.handleAdd}
+              />
+            </li>
+          </ul>
+          <div className={styles.buttons}>
+            <div className={styles.button}>
+              <Button
+                to={AppRoute.EAM}
+                label="Cancel"
+                btnStyle={ButtonStyle.OUTLINED}
+              />
+            </div>
+            <div className={styles.button}>
+              <Button
+                className={styles.button}
+                type={ButtonType.SUBMIT}
+                label={group ? 'Save' : 'Create'}
+              />
+            </div>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export { EAMConfigurateGroup };
