23a675f2f435751da3bd3a14110d1bb144ffd750
JunoJei <jonejei@ukr.net>
2022-03-22 12:49:08 +0200

    BWS-144: + move colors to enum

--- frontend/src/components/common/editor/editor-theme.ts
+++ frontend/src/components/common/editor/editor-theme.ts
@@ -1,113 +1,92 @@
 import { EditorView } from '@codemirror/view';
 import { Extension } from '@codemirror/state';
 import { HighlightStyle, tags } from '@codemirror/highlight';
-
-const CHALKY = '#e5c07b',
-  CORAL = '#e06c75',
-  CYAN = '#56b6c2',
-  INVALID = '#ffffff',
-  IVORY = '#abb2bf',
-  STONE = '#7d8799',
-  MALIBU = '#61afef',
-  SAGE = '#98c379',
-  WHISKEY = '#d19a66',
-  VIOLET = '#c678dd',
-  DARK_BACKGROUND = '#21252b',
-  HIGHLIGHT_BACKGROUND = '#2c313a',
-  BACKGROUND = '#282c34',
-  TOOLTIP_BACKGROUND = '#353a42',
-  SELECTION = '#3E4451',
-  CURSOR = '#528bff',
-  SEARCH_BACKGROUND = '#72a1ff59',
-  SEARCH_OUTLINE = '#457dff',
-  SEARCH_SELECTED_BACKGROUND = '#6199ff2f',
-  SELECTION_MATCH = '#aafe661a',
-  BRACKET = '#bad0f847',
-  BRACKET_OUTLINE = '#515a6b',
-  WHITE = '#ddd',
-  BLACK = '#000';
+import { EditorColor } from 'common/enums/enums';
 
 const Theme = EditorView.theme(
   {
     '&': {
-      color: IVORY,
-      backgroundColor: BACKGROUND,
+      color: EditorColor.IVORY,
+      backgroundColor: EditorColor.BACKGROUND,
       borderRadius: '10px',
       overflowX: 'hidden',
     },
 
     '.cm-content': {
-      caretColor: CURSOR,
+      caretColor: EditorColor.CURSOR,
     },
 
-    '.cm-cursor, .cm-dropCursor': { borderLeftColor: CURSOR },
+    '.cm-cursor, .cm-dropCursor': { borderLeftColor: EditorColor.CURSOR },
     '&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection':
-      { backgroundColor: SELECTION },
+      { backgroundColor: EditorColor.SELECTION },
 
-    '.cm-panels': { backgroundColor: DARK_BACKGROUND, color: IVORY },
+    '.cm-panels': {
+      backgroundColor: EditorColor.DARK_BACKGROUND,
+      color: EditorColor.IVORY,
+    },
     '.cm-panels.cm-panels-top': {
       borderBottomWidth: '2px',
       borderBottomStyle: 'solid',
-      borderBottomColor: BLACK,
+      borderBottomColor: EditorColor.BLACK,
     },
     '.cm-panels.cm-panels-bottom': {
       borderTopWidth: '2px',
       borderTopStyle: 'solid',
-      borderTopColor: BLACK,
+      borderTopColor: EditorColor.BLACK,
     },
 
     '.cm-searchMatch': {
-      backgroundColor: SEARCH_BACKGROUND,
+      backgroundColor: EditorColor.SEARCH_BACKGROUND,
       outlineWidth: '1px',
       outlineStyle: 'solid',
-      outlineColor: SEARCH_OUTLINE,
+      outlineColor: EditorColor.SEARCH_OUTLINE,
     },
     '.cm-searchMatch.cm-searchMatch-selected': {
-      backgroundColor: SEARCH_SELECTED_BACKGROUND,
+      backgroundColor: EditorColor.SEARCH_SELECTED_BACKGROUND,
     },
 
-    '.cm-activeLine': { backgroundColor: HIGHLIGHT_BACKGROUND },
-    '.cm-selectionMatch': { backgroundColor: SELECTION_MATCH },
+    '.cm-activeLine': { backgroundColor: EditorColor.HIGHLIGHT_BACKGROUND },
+    '.cm-selectionMatch': { backgroundColor: EditorColor.SELECTION_MATCH },
 
     '&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket': {
-      backgroundColor: BRACKET,
+      backgroundColor: EditorColor.BRACKET,
       outlineWidth: '1px',
       outlineStyle: 'solid',
-      outlineColor: BRACKET_OUTLINE,
+      outlineColor: EditorColor.BRACKET_OUTLINE,
     },
 
     '.cm-gutters': {
-      backgroundColor: BACKGROUND,
-      color: STONE,
+      backgroundColor: EditorColor.BACKGROUND,
+      color: EditorColor.STONE,
       border: 'none',
     },
 
     '.cm-activeLineGutter': {
-      backgroundColor: HIGHLIGHT_BACKGROUND,
+      backgroundColor: EditorColor.HIGHLIGHT_BACKGROUND,
     },
 
     '.cm-foldPlaceholder': {
       backgroundColor: 'transparent',
       border: 'none',
-      color: WHITE,
+      color: EditorColor.WHITE,
     },
 
     '.cm-tooltip': {
       border: 'none',
-      backgroundColor: TOOLTIP_BACKGROUND,
+      backgroundColor: EditorColor.TOOLTIP_BACKGROUND,
     },
     '.cm-tooltip .cm-tooltip-arrow:before': {
       borderTopColor: 'transparent',
       borderBottomColor: 'transparent',
     },
     '.cm-tooltip .cm-tooltip-arrow:after': {
-      borderTopColor: TOOLTIP_BACKGROUND,
-      borderBottomColor: TOOLTIP_BACKGROUND,
+      borderTopColor: EditorColor.TOOLTIP_BACKGROUND,
+      borderBottomColor: EditorColor.TOOLTIP_BACKGROUND,
     },
     '.cm-tooltip-autocomplete': {
       '& > ul > li[aria-selected]': {
-        backgroundColor: HIGHLIGHT_BACKGROUND,
-        color: IVORY,
+        backgroundColor: EditorColor.HIGHLIGHT_BACKGROUND,
+        color: EditorColor.IVORY,
       },
     },
   },
@@ -115,7 +94,7 @@ const Theme = EditorView.theme(
 );
 
 const Highlight = HighlightStyle.define([
-  { tag: tags.keyword, color: VIOLET },
+  { tag: tags.keyword, color: EditorColor.VIOLET },
   {
     tag: [
       tags.name,
@@ -124,14 +103,20 @@ const Highlight = HighlightStyle.define([
       tags.propertyName,
       tags.macroName,
     ],
-    color: CORAL,
+    color: EditorColor.CORAL,
   },
-  { tag: [tags.function(tags.variableName), tags.labelName], color: MALIBU },
+  {
+    tag: [tags.function(tags.variableName), tags.labelName],
+    color: EditorColor.MALIBU,
+  },
   {
     tag: [tags.color, tags.constant(tags.name), tags.standard(tags.name)],
-    color: WHISKEY,
+    color: EditorColor.WHISKEY,
   },
-  { tag: [tags.definition(tags.name), tags.separator], color: IVORY },
+  {
+    tag: [tags.definition(tags.name), tags.separator],
+    color: EditorColor.IVORY,
+  },
   {
     tag: [
       tags.typeName,
@@ -143,7 +128,7 @@ const Highlight = HighlightStyle.define([
       tags.self,
       tags.namespace,
     ],
-    color: CHALKY,
+    color: EditorColor.CHALKY,
   },
   {
     tag: [
@@ -155,23 +140,23 @@ const Highlight = HighlightStyle.define([
       tags.link,
       tags.special(tags.string),
     ],
-    color: CYAN,
+    color: EditorColor.CYAN,
   },
-  { tag: [tags.meta, tags.comment], color: STONE },
+  { tag: [tags.meta, tags.comment], color: EditorColor.STONE },
   { tag: tags.strong, fontWeight: 'bold' },
   { tag: tags.emphasis, fontStyle: 'italic' },
   { tag: tags.strikethrough, textDecoration: 'line-through' },
-  { tag: tags.link, color: STONE, textDecoration: 'underline' },
-  { tag: tags.heading, fontWeight: 'bold', color: CORAL },
+  { tag: tags.link, color: EditorColor.STONE, textDecoration: 'underline' },
+  { tag: tags.heading, fontWeight: 'bold', color: EditorColor.CORAL },
   {
     tag: [tags.atom, tags.bool, tags.special(tags.variableName)],
-    color: WHISKEY,
+    color: EditorColor.WHISKEY,
   },
   {
     tag: [tags.processingInstruction, tags.string, tags.inserted],
-    color: SAGE,
+    color: EditorColor.SAGE,
   },
-  { tag: tags.invalid, color: INVALID },
+  { tag: tags.invalid, color: EditorColor.INVALID },
 ]);
 
 const EditorTheme: Extension = [Theme, Highlight];
