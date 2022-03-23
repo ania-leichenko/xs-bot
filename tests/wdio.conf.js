97613fda3f582c6f0d869a88b4019401f3656e34
Yaroslaww-1 <yaroslaww1@gmail.com>
2022-03-23 16:26:31 +0200

    BWS-171: * fixed wdio tests not started

--- tests/wdio.conf.js
+++ tests/wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
     //
     specs: [
       //  './test/specs/**/*.ts'
-        './test/specs/**/test.ts'
+        './specs/**/*.ts',
     ],
     // Patterns to exclude.
     exclude: [
