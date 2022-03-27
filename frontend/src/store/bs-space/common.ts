0b08b9d2688780d33c13124f50f7d748b6831d22
Donymak <donyhp2@gmail.com>
2022-03-27 21:05:35 +0300

    BWS-163: - unused actions

--- frontend/src/store/bs-space/common.ts
+++ frontend/src/store/bs-space/common.ts
@@ -2,8 +2,6 @@ enum ActionType {
   GET_OBJECTS = 'bs-space/get-objects',
   DOWNLOAD_OBJECT = 'bs-space/download-object',
   UPLOAD_OBJECT = 'bs-space/upload-object',
-  CLEAR_BLOB = 'bs-space/clear-blob',
-  CLEAR_FORM_DATA = 'bs-space/clear-form-data',
 }
 
 export { ActionType };
