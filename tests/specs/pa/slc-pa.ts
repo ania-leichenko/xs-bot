cf2a8480a07624df2297d1c11f66f8d43bf5fbeb
ViktoriiaShyrokova <vikkiviktorina@gmail.com>
2022-04-01 12:12:49 +0300

    BWS-229: + slc tests

--- tests/specs/pa/slc-pa.ts
+++ tests/specs/pa/slc-pa.ts
@@ -1,4 +1,5 @@
 import { SLC } from '../po/slc-po';
+import { slcData } from '../test-data/slc-data';
 
 const slcAct = new SLC();
 
@@ -27,16 +28,48 @@ class SLCActions {
 
   async ClickDeleteButton(): Promise<void> {
     await slcAct.Delete_Button.waitForExist({
-      timeout: 4000,
+      timeout: 6000,
     });
     await slcAct.Delete_Button.click();
+    await slcAct.DeleteConfirm_Popup.waitForDisplayed({
+      timeout: 6000,
+    });
+    const [DeleteConfirm] = await slcAct.DeleteConfirm_Button;
+    await DeleteConfirm.click();
   }
-
-  async ClickReloadButton(): Promise<void> {
-    await slcAct.Reload_Button.waitForExist({
+  async ClickEditButton(): Promise<void> {
+    await slcAct.Edit_Button.waitForClickable({
+      timeout: 7000,
+    });
+    await slcAct.Edit_Button.click();
+  }
+  async FillTextBox(): Promise<void> {
+    await slcAct.Text_Box.waitForExist({
       timeout: 2000,
     });
-    await slcAct.Reload_Button.click();
+    await slcAct.Text_Box.clearValue();
+    await slcAct.Text_Box.setValue(slcData.FunctionCode);
+  }
+  async ClickSaveFunctionButton(): Promise<void> {
+    const [, SaveFunction] = await slcAct.Function_Buttons;
+    await SaveFunction.waitForClickable({
+      timeout: 3000,
+    });
+    await SaveFunction.click();
+  }
+  async ClickRunFunctionButton(): Promise<void> {
+    const [SaveFunction] = await slcAct.Function_Buttons;
+    await SaveFunction.click();
+  }
+  async FillFunctionArguments(): Promise<void> {
+    await slcAct.Function_Arguments.waitForDisplayed({
+      timeout: 5000,
+    });
+    await slcAct.Function_Arguments.setValue(slcData.FunctionArguments);
+    const [, SaveArguments] = await slcAct.Function_Arguments_Buttons;
+    await SaveArguments.waitForClickable();
+    await SaveArguments.moveTo();
+    await SaveArguments.click();
   }
 }
 
