814a54c6c61bbd14bab7d9dcdce93cec80ef2f60
Ania <ania.leichenko@gmail.com>
2022-05-16 18:45:35 +0300

    XS-BOT: * fix error with telegram token

--- backend/src/services/cron/cron.ts
+++ backend/src/services/cron/cron.ts
@@ -4,6 +4,7 @@ import {
   users as usersService,
 } from '~/services/services';
 import fetch from 'node-fetch';
+import { ENV } from '~/common/enums/enums';
 
 const SUBSCRIPTION_HAS_EXPIRED =
   'Your subscription has expired. In order to continue using our signals, you need to pay for a subscription';
@@ -17,7 +18,7 @@ const task = cron.schedule('0 */1 * * * *', async () => {
       ticket.status === 'Active'
     ) {
       fetch(
-        `https://api.telegram.org/bot5245583761:AAGViUQUROPfgNNSNLLRXK4_GPQ9nUZ3nVw/sendMessage?chat_id=${ticket.chatId}&text=${SUBSCRIPTION_HAS_EXPIRED}`,
+        `https://api.telegram.org/bot${ENV.TELEGRAM_TOKEN}/sendMessage?chat_id=${ticket.chatId}&text=${SUBSCRIPTION_HAS_EXPIRED}`,
         {
           method: 'GET',
           headers: {
@@ -28,7 +29,7 @@ const task = cron.schedule('0 */1 * * * *', async () => {
       );
       for (const admin of admins) {
         fetch(
-          `https://api.telegram.org/bot5245583761:AAGViUQUROPfgNNSNLLRXK4_GPQ9nUZ3nVw/sendMessage?chat_id=${admin.chatId}&text=Expired for ${admin.firstName}, @${admin.username}, ${ticket.plan}`,
+          `https://api.telegram.org/bot${ENV.TELEGRAM_TOKEN}/sendMessage?chat_id=${admin.chatId}&text=Expired for ${admin.firstName}, @${admin.username}, ${ticket.plan}`,
           {
             method: 'GET',
             headers: {
@@ -37,6 +38,7 @@ const task = cron.schedule('0 */1 * * * *', async () => {
             },
           },
         );
+        console.log(`Expired for ${admin.firstName}, @${admin.username}, ${ticket.plan}`);
       }
       await ticketService.updateStatus(ticket.ticket);
     }
